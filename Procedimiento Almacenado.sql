94a93aad9119b9519bf1a010d6bf159d4b1a5b4c
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2020-06-13 21:57:08 -0600

    Add files via upload

---
+++ Procedimiento Almacenado.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE sp_Factura
+	@IdVenta int
+AS
+BEGIN
+SELECT        dbo.Ventas.idVenta, dbo.Ventas.fechadeVenta, dbo.Ventas.totalPagar,  dbo.DetallesVenta.precioCompra, dbo.DetallesVenta.cantidad, dbo.DetallesVenta.totalProducto, 
+                         dbo.ProductosVenta.nombre
+FROM            dbo.ProductosVenta INNER JOIN
+                         dbo.DetallesVenta ON dbo.ProductosVenta.idProductoV = dbo.DetallesVenta.idProductoV INNER JOIN
+                         dbo.Ventas ON dbo.DetallesVenta.idVenta = dbo.Ventas.idVenta
+						 where dbo.Ventas.idVenta = @IdVenta
+END
+GO
